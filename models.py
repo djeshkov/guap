140471905f41280397d141648f28ecc12c154d77
djeshkov <you@example.com>
2015-03-11 20:02:17 +0300

    !

---
+++ models.py
@@ -0,0 +1,92 @@
+from __future__ import absolute_import, print_function
+
+from datetime import datetime
+from decimal import Decimal
+from pony.orm import *
+
+from decimal import Decimal
+from datetime import date
+
+
+
+db = Database("sqlite", "database.sqlite", create_db=True)
+
+class Client(db.Entity):
+    email = Required(unicode, unique=True)
+    password = Required(unicode)
+    name = Required(unicode)
+    country = Required(unicode)
+    address = Required(unicode)
+    orders = Set("Order")
+
+
+class Order(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    state = Required(unicode)
+    date_created = Required(datetime)
+    date_delivered = Optional(datetime)
+    total_price = Required(Decimal)
+    client = Required(Client)
+    items = Set("Order_Item")
+
+
+class Discount(db.Entity):
+    amount = Required(int)
+    date_start = Required(datetime)
+    date_expired = Required(datetime)
+    brand = Required(unicode)
+    clothing = Set("Clothing")
+
+
+class Brand(db.Entity):
+    Name = Required(unicode)
+    country = Required(unicode)
+    description = Optional(unicode)
+    clothing = Set("Clothing")
+
+
+class Category(db.Entity):
+    name = Required(unicode)
+    clothing = Set("Clothing")
+
+
+class Clothing(db.Entity):
+    id = PrimaryKey(int)
+    name = Required(unicode)
+    description = Optional(unicode)
+    picture = Optional(buffer)
+    price = Required(Decimal)
+    category = Required(Category)
+    brand = Required(Brand)
+    store = Optional("Store")
+    order__items = Set("Order_Item")
+    discount = Optional(Discount)
+
+
+class Store(db.Entity):
+    country = Required(unicode)
+    adress = Required(unicode)
+    clothing = Set(Clothing)
+
+
+class Order_Item(db.Entity):
+    quantity = Required(int)
+    price = Required(Decimal)
+    order = Required(Order)
+    clothing = Required(Clothing)
+    PrimaryKey(order, clothing)
+
+
+sql_debug(True)
+db.generate_mapping(create_tables=True)
+
+@db_session
+def populate_database():
+    if select(s for s in Client).count() > 0:
+        return
+
+    s1 = Client(email='js@gmail.com', password='1245', name='John', country='russia', address='192111 Moscow')
+
+
+
+populate_database()
