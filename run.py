307bcf0d2139cac79d41737b198b1789b3c8b9ad
djeshkov <d.jeshkov@gmail.com>
2015-05-20 22:24:12 +0400

    Upload images added, working on storing them in DB

--- run.py
+++ run.py
@@ -5,53 +5,69 @@ from __future__ import with_statement
 from contextlib import closing
 
 # all the imports
+import os
+
+from werkzeug import secure_filename
 import sqlite3
 import json
 from js import *
 from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+    abort, render_template, flash
 
 from models import *
 
- 
+
 # configuration
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
 
+UPLOAD_FOLDER = 'C:\Users\comp-79\PycharmProjects\guap\uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 @app.before_request
 def before_request():
     pass
- 
+
+
 @app.teardown_request
 def teardown_request(exception):
     pass
+
 
 @app.route('/')
 @db_session
 def show_entries():
     return render_template('index.html')
 
+
 @app.route('/admin')
 @db_session
 def show_adminpan():
     return render_template('admin.html')
+
 
 @app.route('/admin/brands')
 @db_session
 def edit_brands():
     brands = Brand.select().order_by(Brand.name)
     return render_template('/admin/brands.html', brands=brands)
+
 
 @app.route('/admin/brands/<id>', methods=['GET', 'POST'])
 @db_session
@@ -62,11 +78,12 @@ def edit_brand_name(id):
         brand.country = request.form['country']
     return render_template('edit_brand.html', brand=brand)
 
+
 @app.route('/admin/brands/create', methods=['GET', 'POST'])
 @db_session
 def create_brand_name():
     if request.method == 'POST':
-        brand = Brand(name=request.form['name'], country= request.form['country'])
+        brand = Brand(name=request.form['name'], country=request.form['country'])
         flush()
         url = url_for('edit_brands')
         return redirect(url)
@@ -83,13 +100,11 @@ def delete_brand_id():
     return redirect(url)
 
 
-
-
-
 @app.route('/admin/clients')
 @db_session
 def edit_clients():
     return render_template('clients.html')
+
 
 @app.route('/admin/clothing')
 @db_session
@@ -98,11 +113,27 @@ def edit_goods():
 
     return render_template('/admin/clothing.html', clothing=clothing)
 
+
 @app.route('/admin/clothing/create', methods=['GET', 'POST'])
 @db_session
 def create_clothing_name():
     if request.method == 'POST':
-        #clothing = Clothing(name=request.form['name'], price= request.form['price'], category= request.form['Category'], brand= request.form['Brand'])
+        # category = Category[request.form['Category']]
+        # brand = Brand(name=request.form['name'], country= request.form['country'])
+        file = request.files['File']
+        #cat= Category[request.form['Category']]
+        #rc = Category(name=request.form['Category'])
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            savepath=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(savepath)
+            #image = Images(name=filename, type="pix", path=savepath, Clothing=request.form['name'])
+            #return redirect(url_for('uploaded_file',
+            #filename=filename))
+        clothing = Clothing(name=request.form['name'], price=request.form['price'], category=request.form['Category'],
+                            brand=request.form['Brand'])
+        #image = Images(name=filename, type="pix", path=savepath, Clothing=clothing)
+
         flush()
         url = url_for('edit_goods')
         return redirect(url)
@@ -112,19 +143,18 @@ def create_clothing_name():
         return render_template('/admin/create_clothing.html', brands=brands, categories=Categories)
 
 
-
-
-
 @app.route('/contacts')
 @db_session
 def index():
     return render_template("contacts.html")
+
 
 @app.route('/catalog')
 @db_session
 def get_categories():
     categories = Category.select().order_by(Category.name)
     return render_template('catalog.html', categories=categories)
+
 
 @app.route('/catalog/<name>')
 @db_session
@@ -134,8 +164,5 @@ def clothing(name):
 
 
 if __name__ == '__main__':
-
-
-
-	app.run(host='0.0.0.0',debug = True)
+    app.run(host='0.0.0.0', debug=True)
 
